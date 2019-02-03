7444ce666067a661872a57881570a64870ef4927
Kaliszewski Michal (CI/DAD4.5) <Michal.Kaliszewski@pl.bosch.com>
2019-02-04 00:14:34 +0100

    strategy and switch time of execution comparison

--- src/test/groovy/StringOperationsSpec.groovy
+++ src/test/groovy/StringOperationsSpec.groovy
@@ -1,3 +1,4 @@
+import org.apache.commons.lang3.RandomStringUtils
 import spock.lang.Specification
 import spock.lang.Unroll
 
@@ -22,13 +23,8 @@ class StringOperationsSpec extends Specification {
 
     @Unroll
     def "Should capitalize string when transform called with CAPITALIZE selector using strategy"(String input, String output) {
-        setup:
-            def start = System.currentTimeMillis()
-
         expect:
             output == new StringOperations().transformWithStrategy(input, StringOperations.StringOperationSelector.CAPITALIZE)
-        cleanup:
-            System.out.println("DEBUG: Logic A took " + (System.currentTimeMillis() - start) + " MilliSeconds")
         where:
             input | output
             "test" | "Test"
@@ -46,17 +42,26 @@ class StringOperationsSpec extends Specification {
         where:
             input | output
             "test" | "Test"
-            "test" | "Test"
-            "test" | "Test"
-            "test" | "Test"
-            "test" | "Test"
-            "test" | "Test"
-            "test" | "Test"
-            "test" | "Test"
-            "test" | "Test"
-            "test" | "Test"
-            "test" | "Test"
-            "test" | "Test"
             "testing CAPITALIZE" | "Testing CAPITALIZE"
+    }
+
+    def "Compare CAPITALIZE time of execution using switch and strategy"() {
+        setup:
+            def start = System.currentTimeMillis()
+        when:
+            start = System.currentTimeMillis()
+            for(int i = 0; i<1000000; i++)
+                new StringOperations().transformWithSwitch(RandomStringUtils.randomAlphabetic(10), StringOperations.StringOperationSelector.LOWER_CASE)
+        then:
+            System.out.println("DEBUG: Switch logic took " + (System.currentTimeMillis() - start) + " MilliSeconds")
+
+        when:
+            start = System.currentTimeMillis()
+            for(int i = 0; i<1000000; i++)
+                new StringOperations().transformWithStrategy(RandomStringUtils.randomAlphabetic(10), StringOperations.StringOperationSelector.LOWER_CASE)
+        then:
+            System.out.println("DEBUG: Strategy logic took " + (System.currentTimeMillis() - start) + " MilliSeconds")
+
+
     }
 }
